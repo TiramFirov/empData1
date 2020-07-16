a00d7a14e7f888ae01c42ed9ce23f0ef5913ff5b
tiram_firov <roman.terekhin@bk.ru>
2020-07-16 09:37:39 +0400

    add surnames on calendar and fix bugs

--- empData/wwwroot/js/calendar-script123.js
+++ empData/wwwroot/js/calendar-script123.js
@@ -1,4 +1,8 @@
-﻿let date = new Date();
+﻿//Логика календаря
+
+let date = new Date();
+
+let lastDayForScreen = 0;
 
 let options = {
 	year: 'numeric',
@@ -6,6 +10,9 @@ let options = {
 	day: 'numeric',
 	weekday: 'long',
 };
+
+let dayWithAbsenteeismOnNormalScreen = document.getElementsByClassName('type-signs-with-FIO');
+let dayWithAbsenteeismOnSmallScreen = document.getElementsByClassName('type-signs-without-FIO');
 
 const renderCalendar = () => {
 	const monthDays = document.querySelector('.days');
@@ -41,26 +48,37 @@ const renderCalendar = () => {
 	let days = "";
 
 	for (let x = firstDayIndex; x > 0; x--) {
-		days += `<div class="prev-date border">${prevLastDay - x + 1}</div>`
+		days += `<div class="prev-date border"><span class="day-num">${prevLastDay - x + 1}</span></div>`
 	}
 
 	for (let i = 1; i <= lastDay; i++) {
 		if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
-			days += `<div data-toggle="modal" data-target="#modal-window" class="today border">${i}</div>`;
+			days += `<div data-toggle="modal" data-target="#modal-window" class="today border"><span class="day-num">${i}</span><div class="type-signs-with-FIO"><span class="type-sign badge badge-warning badge-pill">Перкокуев В.М</span><span class="type-sign badge badge-warning badge-pill">Перкокуев В.М</span></div><div class="type-signs-without-FIO d-none"><span class="type-sign badge badge-warning badge-pill">4</span></div></div></div>`;
 		} else if (i < date.getDate()) {
-			days += `<div data-toggle="modal" data-target="#modal-window" class="border">${i}</div>`;
+			days += `<div data-toggle="modal" data-target="#modal-window" class="border"><span class="day-num">${i}</span><div class="type-signs-with-FIO"><span class="type-sign badge badge-warning badge-pill">Перкокуев В.М</span><span class="type-sign badge badge-warning badge-pill">Перкокуев В.М</span></div><div class="type-signs-without-FIO d-none"><span class="type-sign badge badge-warning badge-pill">5</span></div></div>`;
 		} else {
-			days += `<div class = 'border'>${i}</div>`;
+			days += `<div class="border"><span class="day-num">${i}</span><div class="type-signs-with-FIO"><span class="type-sign badge badge-warning badge-pill">Перкокуев В.М</span><span class="type-sign badge badge-warning badge-pill">Перкокуев В.М</span></div><div class="type-signs-without-FIO d-none"><span class="type-sign badge badge-warning badge-pill">9</span></div></div></div>`;
 		}
 
 	}
 
 	for (let j = 1; j <= nextDays + 1; j++) {
 		if (lastDayIndex != 0) {
-			days += `<div class="next-days border">${j}</div>`;
+			days += `<div class="next-days border"><span class="day-num">${j}</span></div>`;
 		}
 		monthDays.innerHTML = days;
 	}
+
+	lastDayForScreen = lastDay;
+
+
+	if ($(window).width() < 800) {
+		for (let i = 0; i < lastDayForScreen; i++) {
+			dayWithAbsenteeismOnNormalScreen[i].classList.add('d-none');
+			dayWithAbsenteeismOnSmallScreen[i].classList.remove('d-none');
+		}
+	}
+
 }
 
 
@@ -76,3 +94,20 @@ document.querySelector('.next').addEventListener('click', () => {
 });
 
 renderCalendar();
+
+
+
+$(window).resize(() => {
+	if ($(window).width() < 800) {
+		for (let i = 0; i < lastDayForScreen; i++) {
+			dayWithAbsenteeismOnNormalScreen[i].classList.add('d-none');
+			dayWithAbsenteeismOnSmallScreen[i].classList.remove('d-none');
+		}
+
+	} else {
+		for (let i = 0; i < lastDayForScreen; i++) {
+			dayWithAbsenteeismOnNormalScreen[i].classList.remove('d-none');
+			dayWithAbsenteeismOnSmallScreen[i].classList.add('d-none');
+		}
+	}
+});
